97f7d8c966829e74e6e3b4204870873cf1471492
yangyang <yangyang@maihaoche.com>
2017-01-19 18:54:11 +0800

    添加mysql的配置和代码。

---
+++ mysql/config.js
@@ -0,0 +1,9 @@
+var config = {
+    database: 'test',//使用哪个数据库
+    username: 'xianyu',//用户名
+    password:'hello1234',//密码
+    host:'localhost',//主机名
+    port:3306//段欧豪，MySQL默认3306
+}
+
+module.exports = config;
---
+++ mysql/mySQL.js
@@ -0,0 +1,82 @@
+const Sequelizt = require('sequelize');
+const config = require('./config');
+
+var sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 30000
+    }
+});
+
+var Pet = sequelize.define('pet', {
+    id: {
+        type: Sequelize.STRING(50),
+        primaryKey: true
+    },
+    name: Sequelize.STRING(100),
+    gender: Sequelize.BOOLEAN,
+    birth: Sequelize.STRING(10),
+    createdAt: Sequelize.BIGINT,
+    updatedAt: Sequelize.BIGINT,
+    version: Sequelize.BIGINT
+}, {
+        timestamps: false
+    });
+
+var now = Date.now();
+Pet.create({
+    id: 'g-' + now,
+    name: 'Gaffey',
+    gender: false,
+    birth: '2007-07-07',
+    createdAt: now,
+    updatedAt: now,
+    version: 0
+}).then(function (p){
+    console.log('created.' + JSON.stringify(p));
+}).catch(function (err) {
+    console.log('failed: ' + err);
+});
+
+(async () => {
+    var dog = await Pet.create({
+        id: 'd-' + now,
+        name: 'Odie',
+        gender: false,
+        birth: '2008-08-08',
+        createdAt: now,
+        updatedAt: now,
+        version: 0
+    });
+    console.log('created: ' + JSON.stringify(dog));
+})();
+
+(async () => {
+    var pets = await Pet.findAll({
+        where: {
+            name: 'Gaffey'
+        }
+    });
+    console.log(`find ${pets.length} pets:`);
+    for (let p of pets) {
+        console.log(JSON.stringify(p));
+    }
+})();
+
+(async () => {
+    var p = await queryFromSomewhere();
+    p.gender = true;
+    p.updatedAt = Date.now();
+    p.version ++;
+    await p.save();
+})();
+
+(async () => {
+    var p = await queryFromSomewhere();
+    await p.destroy();
+})();
+
+
